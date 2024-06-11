2cb7f3d8b9708afbf3ca74182154970f4a53af8c
niamh <niamh321@me.com>
2024-06-11 11:35:20 +0100

    passwords

--- api/models/user.js
+++ api/models/user.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
   forename: { type: String, required: true },
   surname: { type: String, required: true },
-  username: { type: String, required: true },
-  email: { type: String, required: true },
+  username: { type: String, required: true,
+    // unique: true,
+  },
+  email: { type: String, required: true,
+    // unique: true,
+  },
   password: { type: String, required: true },
   friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   profilePicture: { type: String, default: ""}
